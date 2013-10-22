3d3ee149a9ce4e05ce48a0408c7697918cf58017
orzubalsky <juviley@gmail.com>
2013-10-21 21:37:02 -0400

    removing dajaxice from installed apps

--- interface/settings/base.py
+++ interface/settings/base.py
@@ -52,7 +52,6 @@ STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
     'django.contrib.staticfiles.finders.DefaultStorageFinder',
-    'dajaxice.finders.DajaxiceFinder',
 )
 
 # List of callables that know how to import templates from various sources.
