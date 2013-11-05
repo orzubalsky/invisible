f18002f7496ddf4784ee4f15334cf16c1b4ca527
orzubalsky <juviley@gmail.com>
2013-11-05 11:22:31 -0500

    fixing analytics context processor

--- interface/apps/audiobook/context_processors.py
+++ interface/apps/audiobook/context_processors.py
@@ -1,5 +1,9 @@
 from django.conf import settings
 
+
 def google_analytics(request):
-    if settings.GOOGLE_ANALYTICS_KEY:
-        return { 'GOOGLE_ANALYTICS_KEY' : settings.GOOGLE_ANALYTICS_KEY }
+    try:
+        if settings.GOOGLE_ANALYTICS_KEY:
+            return {'GOOGLE_ANALYTICS_KEY': settings.GOOGLE_ANALYTICS_KEY}
+    except:
+        return {}
